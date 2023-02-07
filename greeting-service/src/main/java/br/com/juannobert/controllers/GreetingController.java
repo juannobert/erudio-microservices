4ca40b847882aa86eb244f618d8563cc366eef49
Juan Nobert <nobert407@gmail.com>
2023-02-07 17:17:09 -0300

    Spring boot actuator and config values

--- greeting-service/src/main/java/br/com/juannobert/controllers/GreetingController.java
+++ greeting-service/src/main/java/br/com/juannobert/controllers/GreetingController.java
@@ -2,25 +2,31 @@ package br.com.juannobert.controllers;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.juannobert.configuration.GreetingConfiguration;
 import br.com.juannobert.models.Greeting;
 
 @RestController
 public class GreetingController {
 
-	private static final String template = "Hello, %s!";
+	private static final String template = "%s, %s!";
 	private final AtomicLong counter = new AtomicLong();
 	
+	@Autowired
+	private GreetingConfiguration greetingConfiguration;
 	@GetMapping("/greeting")
 	public Greeting greeting(
 			@RequestParam(value="name",
-			defaultValue = "World") String name) {
+			defaultValue = "") String name) {
+		
+		if(name.isEmpty()) name = greetingConfiguration.getDefaultValue();
 		return new Greeting(
 					counter.incrementAndGet(),
-					String.format(template, name)
+					String.format(template, greetingConfiguration.getGreeting(),name)
 				);
 	}
 }
