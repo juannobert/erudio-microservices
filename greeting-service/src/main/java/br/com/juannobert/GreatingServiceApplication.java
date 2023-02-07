eb21a536ca510cf2ef85f050140beacce48df713
Juan Nobert <nobert407@gmail.com>
2023-02-07 16:20:20 -0300

    Greeting service created

---
+++ greeting-service/src/main/java/br/com/juannobert/GreatingServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.juannobert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreatingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreatingServiceApplication.class, args);
+	}
+
+}
