274a6cfdbc15e80c198dc2fd08e72b054008dc50
Juan Nobert <nobert407@gmail.com>
2023-02-07 22:02:02 -0300

    Update config server in running

--- greeting-service/src/main/java/br/com/juannobert/configuration/GreetingConfiguration.java
+++ greeting-service/src/main/java/br/com/juannobert/configuration/GreetingConfiguration.java
@@ -1,10 +1,12 @@
 package br.com.juannobert.configuration;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 
 @Component
 @ConfigurationProperties("greeting-service")
+@RefreshScope
 public class GreetingConfiguration {
 	
 	private String greeting;
