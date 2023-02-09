64dd640b50d49700a8082eb7fb611415c8bbb81a
Juan Nobert <nobert407@gmail.com>
2023-02-08 21:58:41 -0300

    Cambio service created

---
+++ cambio-service/src/test/java/br/com/juannobert/CambioServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.juannobert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CambioServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
