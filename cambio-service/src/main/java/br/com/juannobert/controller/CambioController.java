0fd7deed2312a9afefb428f84bfe25565c997af7
Juan Nobert <nobert407@gmail.com>
2023-02-14 10:38:17 -0300

    Retrieving instance port

--- cambio-service/src/main/java/br/com/juannobert/controller/CambioController.java
+++ cambio-service/src/main/java/br/com/juannobert/controller/CambioController.java
@@ -2,6 +2,8 @@ package br.com.juannobert.controller;
 
 import java.math.BigDecimal;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,9 +15,15 @@ import br.com.juannobert.model.Cambio;
 @RequestMapping("/cambio-service")
 public class CambioController {
 
+	
+	@Autowired
+	private Environment environment;
+	
+	
 	@GetMapping("/{amount}/{from}/{to}")
 	public Cambio getCambio(@PathVariable BigDecimal amount,
 			@PathVariable String from,@PathVariable String to) {
-		return new Cambio(1L,from,to,BigDecimal.ONE,BigDecimal.ONE,"PORT 8000");
+		String port = environment.getProperty("local.server.port");
+		return new Cambio(1L,from,to,BigDecimal.ONE,BigDecimal.ONE,port);
 	}
 }
